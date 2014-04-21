8b817694ec48aa60f997c309d0f7fd5a9bda1bd1
Gerii <gerii@server.fake>
2014-04-21 22:09:47 +0200

    Adding Sencha/Corodva project

---
+++ cordova/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/classes.dex : \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/classes/com/freakyworks/muzic/BuildConfig.class \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/classes/com/freakyworks/muzic/R$xml.class \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/classes/com/freakyworks/muzic/R$drawable.class \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/classes/com/freakyworks/muzic/R.class \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/classes/com/freakyworks/muzic/R$attr.class \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/classes/com/freakyworks/muzic/R$string.class \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/classes/com/freakyworks/muzic/Muzic.class \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/dexedLibs/classes-0dba73a791c2c19a008511881b812184.jar \
---
+++ cordova/platforms/android/ant-gen/R.java.d
@@ -0,0 +1,14 @@
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-gen/com/freakyworks/muzic/R.java \
+ : /home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/res/drawable-ldpi/icon.png \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/res/drawable-hdpi/icon.png \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/res/values/strings.xml \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/res/xml/config.xml \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/res/drawable-xhdpi/icon.png \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/res/drawable-mdpi/icon.png \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/res/drawable/icon.png \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/res/drawable/icon.png \
+/home/gery/Dokumente/AptanaWorkspace/sw14_free_02/cordova/platforms/android/ant-build/AndroidManifest.xml \
