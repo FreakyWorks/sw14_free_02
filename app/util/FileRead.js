e9e97b858b491f35b549a7864e9221c166056ca6
Gerii <gerii@server.fake>
2014-05-03 00:29:25 +0200

    [GP,LA]{test after code} Adding directory reader

--- app/util/FileRead.js
+++ app/util/FileRead.js
@@ -3,34 +3,60 @@ Ext.define('Muzic.util.FileRead', {
 	singleton: true,
 	autoDestroy: false,
 	config : {
-        fileSys : undefined
+        fileSys : undefined,
+        dir: undefined
    },
-
-	requestOurFS : function () {
-		//console.log(getEventListener(document));
-		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, this.gotFileSystem, this.fail);
-		
-	},
+   
 	constructor : function(config) {
 		this.initConfig(config);
 		this.callParent([config]);
 	},
-	
+
+	//Request a file system
+	requestOurFS : function () {
+		//console.log(getEventListener(document));
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, this.gotFileSystem, this.didntGetFileSystem);
+		
+	},
+
+	//Filesystem successfully requested
 	gotFileSystem : function (fileSystem) {
 		console.log("gotFS");
 		Muzic.util.FileRead.setFileSys(fileSystem);
-		
+		//TEST:
+		console.log(fileSystem.root);
+		/*var dirReader = fileSystem.root.createReader();
+		console.log("hier");
+		dirReader.readEntries(this.readEntries, this.fail);*/
+	},
+	
+	//Request a directory, path should be relative to root
+	requestDir : function (folder) {
+		console.log("requestingDir: " + folder);
+		var fileSystem = Muzic.util.FileRead.getFileSys();
+		fileSystem.root.getDirectory(folder, {create: false, exclusive: false}, this.gotDirectory, this.logErrorCode);
+	},
+	
+	//Muzic.util.FileRead.getFileSys().root.createReader().readEntries(Muzic.util.FileRead.gotDirectory, Muzic.util.FileRead.fail);
+	
+	//Directory successfully requested
+	gotDirectory : function (directory) {
+		console.log("gotDir");
+		Muzic.util.FileRead.setDir(directory);
+		console.log(directory);
 	},
 	
 
 	
 	
-	//Our error handler
-	fail : function (message) {
+	
+	
+	//Our error handlers
+	didntGetFileSystem : function (err) {
 		//TODO show fail to user
-		console.log('An error has occured in: ' + message);
+		console.log('An error has occured: ' + err.target.error.code);
+	},
+	logErrorCode : function (err) {
+		console.log('An error has occured: ' + err.code + ' - See https://developer.mozilla.org/en-US/docs/Web/API/FileError');
 	}
 });
-
-//This is already listened to by Sencha!
-//document.addEventListener('deviceready', file.requestOurFS() , false);
