72c9fe6291b13635e8d3b30afc42b99773f00ed3
Gerii <gerii@server.fake>
2014-05-03 15:03:57 +0200

    [GP,LA]{test before code} Adding tests for store loader

--- unittests/app/util/FileRead.js
+++ unittests/app/util/FileRead.js
@@ -1,66 +1,106 @@
 Ext.require('Muzic.util.FileRead');
 
 
-
-describe("Filesytem", function () {
-	  beforeEach(function(done) {
-	  	Muzic.util.FileRead.requestOurFS();
-	    setTimeout(function() {
-	      done();
-	    }, 2000);
-	  });
-	  
-	it("successfully requested", function(done) {
-	    expect(Muzic.util.FileRead.getFileSys()).toBeDefined();
-		done();
+describe("Muzic.util.FileRead", function () {
+	describe("Filesytem", function () {
+		  beforeEach(function(done) {
+		  	Muzic.util.FileRead.requestOurFS();
+		    setTimeout(function() {
+		      done();
+		    }, 2000);
+		  });
+		  
+		it("successfully requested in under 2s", function(done) {
+		    expect(Muzic.util.FileRead.getFileSys()).toBeDefined();
+			done();
+		});
+		
+		afterEach(function () {
+	    	//Muzic.util.FileRead.setFileSys(undefined);
+		});
 	});
 	
-	afterEach(function () {
-    	//Muzic.util.FileRead.setFileSys(undefined);
-	});
-});
-
-describe("Get Directory", function () {
-	 beforeEach(function(done) {
-	  	Muzic.util.FileRead.requestDir('Music');
-	    setTimeout(function() {
-	      done();
-	    }, 2000);
-	});
-	  
-	it("has gotten our music directory", function(done) {
-		var dir = Muzic.util.FileRead.getDir();
-		console.log(dir);
-	    expect(dir).toBeDefined();
-	    expect(Muzic.util.FileRead.getDir().name).toBe('Music');
-		done();
-	});
-	
-	afterEach(function () {
-    	//Muzic.util.FileRead.setDir(undefined);
-	});
-});
-
-
-describe("Directory Reader", function () {
-	 beforeEach(function(done) {
-	  	Muzic.util.FileRead.requestEntries();
-	    setTimeout(function() {
-	      done();
-	    }, 2000);
-	});
-	  
-	it("has gotten the entries of our Music directory", function(done) {
-		var dirEntries = Muzic.util.FileRead.getDirEntries();
-		console.log(dirEntries);
-	    expect(dirEntries).toBeDefined();
-	    expect(dirEntries).toContain(jasmine.objectContaining({
-      		isFile: true
-    	}));
-		done();
+	describe("Get Directory", function () {
+		 beforeEach(function(done) {
+		  	Muzic.util.FileRead.requestDir('Music');
+		    setTimeout(function() {
+		      done();
+		    }, 2000);
+		});
+		  
+		it("has gotten our music directory in under 2s", function(done) {
+			var dir = Muzic.util.FileRead.getDir();
+			console.log(dir);
+		    expect(dir).toBeDefined();
+		    expect(Muzic.util.FileRead.getDir().name).toBe('Music');
+			done();
+		});
+		
+		afterEach(function () {
+	    	//Muzic.util.FileRead.setDir(undefined);
+		});
 	});
 	
-	afterEach(function () {
-    	Muzic.util.FileRead.setDir(undefined);
+	//TODO try to merge it now
+	describe("Directory Reader", function () {
+		 beforeEach(function(done) {
+		  	Muzic.util.FileRead.requestEntries();
+		    setTimeout(function() {
+		      done();
+		    }, 2000);
+		});
+		  
+		it("has gotten the entries of our Music directory in under 2s", function(done) {
+			var dirEntries = Muzic.util.FileRead.getDirEntries();
+			console.log(dirEntries);
+		    expect(dirEntries).toBeDefined();
+		    expect(dirEntries).toContain(jasmine.objectContaining({
+	      		isFile: true
+	    	}));
+			done();
+		});
+		
+		afterEach(function () {
+	    	Muzic.util.FileRead.setDir(undefined);
+		});
 	});
+
+
+
+	describe("Store", function () {
+		var store;
+		 beforeEach(function(done) {
+		 	jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+		  	store = Muzic.util.FileRead.getStore('blablabla');
+		  	console.log(store);
+		    setTimeout(function() {
+		      done();
+		    }, 4000);
+		});
+		
+		it("should not load", function(done) {
+			expect(store).toBeUndefined();
+			done();
+		});
+	});
+	describe("Store", function () {
+		var store;
+		 beforeEach(function(done) {
+		 	jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+		  	store = Muzic.util.FileRead.getStore('Songs');
+		  	console.log(store);
+		    setTimeout(function() {
+		      done();
+		    }, 4000);
+		});
+		
+		it("Songs has been loaded", function(done) {
+			expect(store).toBeDefined();
+			expect(store.getStoreId()).toBe('Songs');
+			done();
+		});
+	});
+
+
 });
+
