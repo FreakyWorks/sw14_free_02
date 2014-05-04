58866279b9a8fd31957c7c189051e0b676b4d3d3
Gerii <Gerii@users.noreply.github.com>
2014-05-04 17:45:41 +0200

    [GP,MH]{test before code} Add test for audio toggler

--- unittests/app/controller/Songs.js
+++ unittests/app/controller/Songs.js
@@ -3,14 +3,24 @@ describe('Muzic.controller.Songs', function () {
   beforeEach(function () {
     app = Ext.create('Ext.app.Application', {name: 'Muzic'});
     controller = Ext.create('Muzic.controller.Songs', { application: app });
+    controller.init();
     controller.launch();
   });
 
-  it('#play', function () {
-  	var audio = Ext.create('Ext.Audio');
-    var order = controller.onPauseButtonTap();
-    expect(controller.onPauseButtonTap).toEqual('Muzic.model.Song');
-    expect(order.phantom).toBeTruthy();
+  it('should stop playing', function () {
+  	//var audio = Ext.create('Ext.Audio');
+  	//audio.setUrl('file:///storage/sdcard/Music/crash.mp3');
+  	controller.getAudioPlayer().play();
+  	console.log(controller.getAudioPlayer());
+  	console.log(controller.getAudioPlayer().isPlaying());
+  	console.log(controller);
+    controller.toggleAudioPlayback(controller.getAudioPlayer());
+    console.log(controller.getAudioPlayer());
+    console.log(controller.getAudioPlayer().isPlaying());
+    console.log("ende");
+    expect(controller.getAudioPlayer().isPlaying()).toBeFalsy;
+    /*expect(controller.onPauseButtonTap).toEqual('Muzic.model.Song');
+    expect(order.phantom).toBeTruthy();*/
   });
   /*it('#newOrder', function () {
   	var audio = Ext.create('Ext.Audio');
@@ -21,5 +31,6 @@ describe('Muzic.controller.Songs', function () {
  
   afterEach(function () {
     app.destroy();
+    document.getElementById('mytest').innerHTML = "";
   });
 });
