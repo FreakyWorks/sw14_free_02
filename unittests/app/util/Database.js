b10911980f355eb0a3e607208c581ad1e4237e0e
Gerii <Gerii@users.noreply.github.com>
2014-05-24 22:57:29 +0200

    [GP,LA]{test before code} Add table creator

--- unittests/app/util/Database.js
+++ unittests/app/util/Database.js
@@ -3,13 +3,22 @@ Ext.require('Muzic.util.Database');
 
 describe("Muzic.util.Database", function () {
 	describe("Database loader", function () {
-	
-	
 	    it("has loaded our database", function () {
 	    	Muzic.util.Database.openDB();
 	        expect(Muzic.util.Database.getDatabase()).toBeDefined();
 	    });
+	});
 	
+	describe("Table creator", function () {
+	    it("has created our tables", function () {
+	    	Muzic.util.Database.createTables();
+	    	myDb = Muzic.util.Database.getDatabase();
+	    	myDb.transaction(function (tx) {
+			   tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='songs_table';", [], function (tx, results) {
+			   expect(results.rows.length).toBe(1);
+			 }, null);
+			});
+	    });
 	});
 });
 
