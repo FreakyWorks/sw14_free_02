da2fb669f567f06d2e60645b6d11350bba404109
Gerii <Gerii@users.noreply.github.com>
2014-05-24 23:08:10 +0200

    [GP,LA]{test after code} Add table creator

--- app/util/Database.js
+++ app/util/Database.js
@@ -13,7 +13,19 @@ Ext.define('Muzic.util.Database', {
 
 	openDB : function () {
 		Muzic.util.Database.setDatabase(window.sqlitePlugin.openDatabase({name: "MusicDatabase"}));
-	}
+	},
+	
+	createTables: function () {
+		if (Muzic.util.Database.getDatabase() == undefined) {
+			Muzic.util.Database.openDB();
+		}
+		myDb = Muzic.util.Database.getDatabase();
+		myDb.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS songs_table (song_id integer primary key, title text, filepath text)');
+		//TODO add table for artist, album if id3 reader works
+      });
+	},
+
 
 
 });
