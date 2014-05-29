24765a67d9faa3cc31bebf0a579ecc7c61e09d98
Gerii <Gerii@users.noreply.github.com>
2014-05-29 14:49:54 +0200

    [GP.LA]{other} Set up Siesta, add first test

--- uitests/010_sanity.t.js
+++ uitests/010_sanity.t.js
@@ -2,11 +2,10 @@ StartTest(function(t) {
     t.diag("Sanity");
 
     t.ok(Ext, 'ExtJS is here');
-    t.ok(Ext.Window, '.. indeed');
 
 
-    t.ok(Your.Project, 'My project is here');
-    t.ok(Your.Project.Util, '.. indeed');
+    t.ok(Muzic, 'My project is here');
+    t.ok(Muzic.util.FileRead, '.. indeed');
 
     t.done();   // Optional, marks the correct exit point from the test
-})
+});
