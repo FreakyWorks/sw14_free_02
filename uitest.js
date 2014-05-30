3fa57630f761c447ae5b2a94a1f311280c140bb0
Gerii <Gerii@users.noreply.github.com>
2014-05-30 22:50:44 +0200

    [GP,LA]{other}set up project to use Chrome/Ripple for UI testing, write sanity tests

--- uitest.js
+++ uitest.js
@@ -4,25 +4,33 @@ Harness.configure({
     title       : 'Muzic Test Suite',
 
     preload     : [
+	    {
+	        text    : 'document.addEventListener("DOMContentLoaded", function(event) { var el = document.createElement("div"); el.id = "appLoadingIndicator"; document.body.appendChild(el) })'
+	    },
+    	//'cordova.js',
         // version of ExtJS used by your application
-        '../touch/resources/css/base.css',
-        //'../resources/css/app.css',
+        'touch/resources/css/base.css',
+        'resources/css/app.css',
 
         // version of ExtJS used by your application
-        '../touch/sencha-touch-all-debug.js',
-        //'../app.js'
-    ]
+        'touch/sencha-touch-all-debug.js',
+        'app.js'
+    ],
+    loaderPath  : { 'Muzic' : 'app' },
+	performSetup: false/*,
+    keepNLastResults    : 2*/
 });
 
 Harness.start(
-    '010_sanity.t.js',
-    '020_basic.t.js'/*,
-    {
+	'uitests/010_sanity.t.js',
+	'uitests/030_player.t.js',
+	'uitests/020_titlelist.t.js'
+    /*{
         group           : 'Application tests',
-        hostPageUrl     : '../',
+        hostPageUrl     : './',
         performSetup    : false,       // This is done by the app itself
         items           : [
-            'DemoApp/tests/100_sanity.t.js',
+            '010_sanity.t.js'/*
             'DemoApp/tests/101_login.t.js',
             'DemoApp/tests/102_logout.t.js'
         ]
--- uitests/010_sanity.t.js
+++ uitests/010_sanity.t.js
@@ -1,3 +1,4 @@
+testUI = true;
 StartTest(function(t) {
     t.diag("Sanity");
 
@@ -6,6 +7,19 @@ StartTest(function(t) {
 
     t.ok(Muzic, 'My project is here');
     t.ok(Muzic.util.FileRead, '.. indeed');
+
+    t.chain(
+        { 
+            waitFor : 'CQ', 
+            args    : 'player',
+            desc    : 'Should find songs view on app start'
+        },
+        
+        function(next) {
+            t.ok(t.cq1('#pauseButton'), 'Should find a song list');
+        }
+    );
+
 
     t.done();   // Optional, marks the correct exit point from the test
 });
