9d06181b49dcbd066ebc68252fbbf135e187ad6a
Gerii <Gerii@users.noreply.github.com>
2014-05-31 13:14:12 +0200

    [GP,LA]{test before code} Add test for titlelist

---
+++ uitests/020_titlelist.t.js
@@ -0,0 +1,37 @@
+testUI = true;
+StartTest(function (t) {
+    t.diag('Testing our songs list');
+    
+    t.chain(
+
+    	{ waitFor : 1000 },
+    	
+        function(next) {
+
+            t.scrollUntilElementVisible(t.cq1('#mySongList'), 'down', '.x-list-item-last', next);
+        },
+
+        function(next) {
+            t.pass('Scrolled last item into view');
+            next();
+        },
+
+        { action : 'tap', target : '.x-list-item-last' },
+
+        function(next) {
+            var sel = t.cq1('#mySongList').getSelection();
+            console.log(sel[0]);
+            console.log("ee");
+            t.is(sel.length, 1, '1 item is selected');
+            t.is(sel[0].data.filepath, 'music_files/crash.mp3', 'crash.mp3 is selected');
+
+            // Let's go back up
+            t.scrollUntilElementVisible(t.cq1('#mySongList').element, 'up', t.cq1('#mySongList').element.down('.x-list-item-first'), next);
+        	
+        	button_text = t.cq1('#pauseButton').textElement.dom.innerHTML;
+        	t.is(button_text, "Pause", 'Button text is Pause after clicking element in list');
+        	t.is(t.cq1('audio').isPlaying(), true, "Audio is playing");
+            t.done();
+        }
+    );
+});
