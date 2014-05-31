6671a35a99e9aa30f49fe79df51ccd4f47b0d1ec
Gerii <Gerii@users.noreply.github.com>
2014-05-31 15:20:10 +0200

    [GP,LA]{test before code} Add song on startup to player

--- uitests/030_player.t.js
+++ uitests/030_player.t.js
@@ -7,6 +7,7 @@ StartTest(function(t) {
 	t.willFireNTimes(button[0], 'tap', 2, 'Click Pause Button two times');
     t.chain(
         { waitFor : 'componentVisible', args : '#pauseButton' },
+        { waitFor : 2000},
 		
        function(next) {
         	button_text = t.cq1('#pauseButton').textElement.dom.innerHTML;
@@ -15,10 +16,13 @@ StartTest(function(t) {
             next();
         },
         
-        
 		{ tap : '>> #pauseButton' },
 		
         function(next) {
+			var should_path = Ext.getStore('Songs').first().getData().filepath;
+        	var is_path = t.cq1('audio').getUrl( );
+        	console.log(is_path);
+        	t.is(is_path, should_path, 'First element in list is selected for playing');
         	button_text = t.cq1('#pauseButton').textElement.dom.innerHTML;
         	t.is(button_text, "Pause", 'Button text is Pause after pressing');
         	t.is(t.cq1('audio').isPlaying(), true, 'Audio is playing after button press');
